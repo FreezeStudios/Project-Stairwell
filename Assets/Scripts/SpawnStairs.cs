1db3e3b890045732f6763deaeb55b35336ab6dea
freeze_studios <pradyumgautam.pg@gmail.com>
2019-04-16 13:49:50 +0530

    Map Generation Completed TODO : Chance based and time based jumpscares. Spawn blood on stairs

---
+++ Assets/Scripts/SpawnStairs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStairs : MonoBehaviour
+{
+    [SerializeField] private GameObject stairsPrefab;
+    [SerializeField] private Transform stairwell;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Collider>().tag == "Player")
+        {
+            Quaternion rot = Quaternion.Euler(0, 0, 0);
+            int stairwellChildCount = stairwell.childCount - 1;
+            Vector3 pos = stairwell.GetChild(stairwellChildCount).transform.position;
+            Vector3 modPos = new Vector3(pos.x,pos.y -5f, pos.z);
+            GameObject go = Instantiate(stairsPrefab, modPos, rot,stairwell);
+            go.name = "Full_Stairs " + stairwellChildCount.ToString();
+            Destroy(gameObject);
+        }
+    }
+}
