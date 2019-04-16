7bdf0e868987ee3deb0e7cf0dddf4ede8e683537
freeze_studios <pradyumgautam.pg@gmail.com>
2019-04-16 23:12:12 +0530

    Floor Generation TO DO - Jumpscares, spawning blood, 2D Jumpscares w/ Pictures @ Last

---
+++ Assets/Scripts/SpawnStairs.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStairs : MonoBehaviour
+{
+    [SerializeField] private GameObject stairsPrefab;
+    private Transform stairwell;
+
+    private void Start()
+    {
+        stairwell = GameObject.FindGameObjectWithTag("Stairwell").GetComponent<Transform>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Collider>().tag == "Player")
+        {
+            Quaternion rot = Quaternion.Euler(0, 0, 0);
+            int stairwellChildCount = stairwell.childCount - 1;
+            Vector3 pos = stairwell.GetChild(stairwellChildCount).transform.position;
+            Vector3 modPos = new Vector3(pos.x,pos.y -5f, pos.z);
+            GameObject go = Instantiate(stairsPrefab, modPos, rot,stairwell);
+            go.name = "Full_Stairs " + stairwellChildCount.ToString();
+            Destroy(gameObject);
+        }
+    }
+}
